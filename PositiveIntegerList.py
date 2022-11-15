d2ae3cac1c585822b9926f948211aeb068a10085
Jibi <jibi.b@credot.dev>
2022-11-15 20:46:44 +0530

    List of positive integer numbers

--- PositiveIntegerList.py
+++ PositiveIntegerList.py
@@ -1,4 +1,4 @@
-InitialList = [[22, -17, -22], [-68, 34, -12, -47, 8, 99]]
+InitialList = [[22, -17, -22], [-68, 34, -12, -47, 8, 52,  99]]
 FinalList = [[a for a in b if a > 0] for b in InitialList]
 print("Initial List is :", InitialList)
 print("Modified list is :", FinalList)
